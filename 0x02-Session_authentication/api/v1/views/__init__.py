ac04c74f6496a8bd640e547330abcf5f863714c9
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-05-23 15:25:03 +0200

    Update __init__.py

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
+"""Blueprint for API routes.
+"""
+from flask import Blueprint
 
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+from api.v1.views.index import *
+from api.v1.views.users import *
+
+# Load users from file when the blueprint is initialized.
+User.load_from_file()
+
+from api.v1.views.session_auth import *
