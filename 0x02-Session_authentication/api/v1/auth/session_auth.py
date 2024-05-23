9dabe7649279c333eacb1f29e45d4834977df422
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-05-23 15:12:24 +0200

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,37 +9,36 @@ from models.user import User
 
 
 class SessionAuth(Auth):
-    """Session authentication class.
+    """Manages session authentication.
     """
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Creates a session id for the user.
+        """Creates a session ID for a user.
         """
-        if type(user_id) is str:
+        if isinstance(user_id, str):
             session_id = str(uuid4())
             self.user_id_by_session_id[session_id] = user_id
             return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Retrieves the user id of the user associated with
-        a given session id.
+        """Retrieves the user ID associated with a session ID.
         """
-        if type(session_id) is str:
+        if isinstance(session_id, str):
             return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None) -> User:
-        """Retrieves the user associated with the request.
+        """Retrieves the user based on the session ID in the request.
         """
         user_id = self.user_id_for_session_id(self.session_cookie(request))
         return User.get(user_id)
 
-    def destroy_session(self, request=None):
+    def destroy_session(self, request=None) -> bool:
         """Destroys an authenticated session.
         """
         session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session_id)
-        if (request is None or session_id is None) or user_id is None:
+        if request is None or session_id is None or user_id is None:
             return False
         if session_id in self.user_id_by_session_id:
             del self.user_id_by_session_id[session_id]
