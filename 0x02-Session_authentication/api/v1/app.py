2764d4c99582df880644b9ac0753e1bbaa7cffe5
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-05-23 15:15:58 +0200

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -4,7 +4,7 @@
 import os
 from os import getenv
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS
 
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
@@ -13,47 +13,48 @@ from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from api.v1.auth.session_db_auth import SessionDBAuth
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 auth = None
 auth_type = getenv('AUTH_TYPE', 'auth')
+
 if auth_type == 'auth':
     auth = Auth()
-if auth_type == 'basic_auth':
+elif auth_type == 'basic_auth':
     auth = BasicAuth()
-if auth_type == 'session_auth':
+elif auth_type == 'session_auth':
     auth = SessionAuth()
-if auth_type == 'session_exp_auth':
+elif auth_type == 'session_exp_auth':
     auth = SessionExpAuth()
-if auth_type == 'session_db_auth':
+elif auth_type == 'session_db_auth':
     auth = SessionDBAuth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """Handles 404 Not Found errors
+    """Handles 404 Not Found errors.
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """Handles 401 Unauthorized errors
+    """Handles 401 Unauthorized errors.
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """It handles 403 Forbidden errors
+    """Handles 403 Forbidden errors.
     """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
 def authenticate_user():
-    """It authenticates user before processing any request
+    """Authenticates user before processing any request.
     """
     if auth:
         excluded_paths = [
