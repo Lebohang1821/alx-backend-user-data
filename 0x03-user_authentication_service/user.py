3e808dd63978c70a0d1b5bfeab947da885b02235
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-05-28 17:20:02 +0200

    Update user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,9 +4,7 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
-
 
 class User(Base):
     """It represents user record in users table.
