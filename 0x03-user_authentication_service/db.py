0114c20e459e41abafd9b65edef7ac717a3f7368
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-05-28 17:38:59 +0200

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,49 +32,49 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-def add_user(self, email: str, hashed_password: str) -> User:
-    """Creates a new user and adds it to the database.
-    """
-    try:
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Adds a new user to the database.
+        """
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user based on a set of filters.
+        """
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates a user based on a given id.
+        """
+        user = self.find_user_by(id=user_id)
+        if user is None:
+            return
+        update_source = {}
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                update_source[getattr(User, key)] = value
+            else:
+                raise ValueError()
+        self._session.query(User).filter(User.id == user_id).update(
+            update_source,
+            synchronize_session=False,
+        )
         self._session.commit()
-    except Exception:
-        self._session.rollback()
-        new_user = None
-    return new_user
-
-def find_user_by(self, **kwargs) -> User:
-    """Searches for a user using specified filters.
-    """
-    fields, values = [], []
-    for key, value in kwargs.items():
-        if hasattr(User, key):
-            fields.append(getattr(User, key))
-            values.append(value)
-        else:
-            raise InvalidRequestError()
-    result = self._session.query(User).filter(
-        tuple_(*fields).in_([tuple(values)])
-    ).first()
-    if result is None:
-        raise NoResultFound()
-    return result
-
-def update_user(self, user_id: int, **kwargs) -> None:
-    """Modifies a user's details based on their id.
-    """
-    user = self.find_user_by(id=user_id)
-    if user is None:
-        return
-    update_source = {}
-    for key, value in kwargs.items():
-        if hasattr(User, key):
-            update_source[getattr(User, key)] = value
-        else:
-            raise ValueError()
-    self._session.query(User).filter(User.id == user_id).update(
-        update_source,
-        synchronize_session=False,
-    )
-    self._session.commit()
