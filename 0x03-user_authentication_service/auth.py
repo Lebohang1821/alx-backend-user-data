9f84114d81863b47a503faac575a8d7c8a6be14d
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-05-29 15:05:10 +0200

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,7 +83,7 @@ class Auth:
         self._db.update_user(user_id, session_id=None)
 
     def get_reset_password_token(self, email: str) -> str:
-        """It generates a password reset token for user."""
+        """It generates password reset token for user."""
         user = None
         try:
             user = self._db.find_user_by(email=email)
