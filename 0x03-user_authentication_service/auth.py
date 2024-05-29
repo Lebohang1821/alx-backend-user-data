a8c8403481278d9d2a461c4eba2583fa6e99fc7c
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-05-29 15:04:53 +0200

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -53,7 +53,7 @@ class Auth:
         return False
 
     def create_session(self, email: str) -> str:
-        """Creates a new session for the user."""
+        """It creates new session for the user."""
         user = None
         try:
             user = self._db.find_user_by(email=email)
